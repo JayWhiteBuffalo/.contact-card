71786cd52f63d8cae816cc58b3747243cc6a9fdc
White Buffalo <apothocare@gmail.com>
2022-11-21 17:10:13 -0600

    add PWA get and post functions

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,9 +1,12 @@
 // Import modules
 import  "./form";
 import "./submit";
+  // Import CSS files
+  import "../css/index.css";
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./database";
+import {postDb, getDb, initDb} from "./database";
+
 
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
@@ -11,13 +14,15 @@ import Dog from '../images/dog.png';
 
 //to insert the images into the page
 window.addEventListener('load', function () {
+    initDb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
-    initDb();
+// We are temporarily placing getDb() and postDb() function calls here for testing. We will move it to another event listener later.
+    getDb();
+    postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
+    getDb();
     
   });
 
-  // Import CSS files
-import "../css/index.css";
-import { initDb } from "./database";
+
