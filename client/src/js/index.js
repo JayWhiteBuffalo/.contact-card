ad592d750d28abe9e7f5766011ae2b5a134c568c
White Buffalo <apothocare@gmail.com>
2022-11-22 00:45:21 -0600

    SW added by using workbox GenerateSW

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -8,7 +8,6 @@ import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
